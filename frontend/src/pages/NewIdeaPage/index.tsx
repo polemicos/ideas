053509295f03f8260819f490addadddbca2a64d7
Mikita Rakovich <nikitarakovich61@gmail.com>
2025-06-06 00:54:48 +0200

    Added basic formik

--- frontend/src/pages/NewIdeaPage/index.tsx
+++ frontend/src/pages/NewIdeaPage/index.tsx
@@ -1,43 +1,44 @@
-import { useState } from 'react'
+import { useFormik } from 'formik'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
 import { Textarea } from '../../components/Textarea'
 
 export const NewIdeaPage = () => {
-  const [state, setState] = useState({
-    title: '',
-    nick: '',
-    description: '',
-    text: '',
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+      text: '',
+    },
+    onSubmit: (values) => {
+      console.info('Submitted', values)
+    },
   })
 
   return (
     <Segment title="New Idea">
       <form
         onSubmit={(e) => {
-          e.preventDefault()
-          console.info('Submitted', state)
+          e.preventDefault();
+          formik.handleSubmit()
         }}
       >
         <Input 
           name={'title'} 
           label={'Title for your idea'} 
-          state={state} 
-          setState={setState}
+          formik={formik}
         />
         
         <Input 
           name={'description'} 
           label={'Describe your idea'} 
-          state={state} 
-          setState={setState}
+          formik={formik}
         />
 
         <Textarea
           name={'text'} 
           label={'Write your idea'} 
-          state={state} 
-          setState={setState} 
+          formik={formik}
         />
 
         <button type="submit">Create Idea</button>
