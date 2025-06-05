8e193f520ab05bdd9295e4eda55cff00de2319b0
Mikita Rakovich <nikitarakovich61@gmail.com>
2025-06-06 00:40:25 +0200

    Added form for NewIdeaView

--- frontend/src/pages/NewIdeaPage/index.tsx
+++ frontend/src/pages/NewIdeaPage/index.tsx
@@ -1,10 +1,47 @@
-import css from './index.module.scss';
+import { useState } from 'react'
+import { Input } from '../../components/Input'
+import { Segment } from '../../components/Segment'
+import { Textarea } from '../../components/Textarea'
 
 export const NewIdeaPage = () => {
+  const [state, setState] = useState({
+    title: '',
+    nick: '',
+    description: '',
+    text: '',
+  })
+
   return (
-    <div>
-      <h1 className={css.title}>New Idea</h1>
-      <div>Form will be here soon</div>
-    </div>
-  );
-};
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          console.info('Submitted', state)
+        }}
+      >
+        <Input 
+          name={'title'} 
+          label={'Title for your idea'} 
+          state={state} 
+          setState={setState}
+        />
+        
+        <Input 
+          name={'description'} 
+          label={'Describe your idea'} 
+          state={state} 
+          setState={setState}
+        />
+
+        <Textarea
+          name={'text'} 
+          label={'Write your idea'} 
+          state={state} 
+          setState={setState} 
+        />
+
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
+  )
+}
