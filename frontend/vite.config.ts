da727caebd19e2b28119541b703512d45bcfae17
Mikita Rakovich <nikitarakovich61@gmail.com>
2025-06-07 14:14:10 +0200

    Changed depricated scss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: [
+          path.resolve(__dirname, 'src/styles'),
+          path.resolve(__dirname, 'node_modules'),
+        ],
+        api: 'legacy',
+      },
+    },
+  },
 });
